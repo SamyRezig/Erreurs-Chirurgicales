f10c33e21058d500f04b114a8c77db81b7a28419
tranyv16 <tranyv16@A204-W10-26.windows.dauphine.fr>
2019-01-21 12:45:53 +0100

    Ajout de la classe IntervalleTemps

---
+++ JavaChirurgie/src/IntervalleTemps.java
@@ -0,0 +1,26 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class IntervalleTemps {
+	private LocalDateTime dateDebut;
+	private LocalDateTime dateFin;
+	
+	public IntervalleTemps(String jourDebut, String horaireDebut, String jourFin, String horaireFin) {
+		// Concatenation des String pour avoir deux dates de la forme dd/MM/yyyy HH:mm:ss distincts
+		String dateDebut = jourDebut + " " + horaireDebut;
+		String dateFin = jourFin + " " + horaireFin;
+		
+		// Definition du formatteur pour convertir les String en LocalDateTime
+		DateTimeFormatter formatteur = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+		
+		// Conversion et setting
+		this.dateDebut = LocalDateTime.parse(dateDebut, formatteur);
+		this.dateFin = LocalDateTime.parse(dateFin,  formatteur);
+	}
+	
+	@Override
+	public String toString() {
+		return (this.dateDebut + "--" + this.dateFin);
+	}
+	
+}
