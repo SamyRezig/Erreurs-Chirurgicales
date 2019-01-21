c733b6e9cbfd459e86ca1c03603f3c1b5f68bd74
tranyv16 <tranyv16@A204-W10-26.windows.dauphine.fr>
2019-01-21 13:22:56 +0100

    Ajout classe Ubiquite

---
+++ JavaChirurgie/src/Ubiquite.java
@@ -0,0 +1,15 @@
+
+public class Ubiquite extends Conflit {
+
+	
+
+	public Ubiquite(Chirurgie first, Chirurgie second) {
+		super(first, second);
+  	}
+
+	@Override
+	public void resoudreConflit(Chirurgie first, Chirurgie second) {
+		
+	}
+
+}
