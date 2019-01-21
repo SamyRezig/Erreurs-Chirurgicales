5a8b15f2e6ac0f8a2ed3dfb73f5b4136ded47416
tranyv16 <tranyv16@A204-W10-26.windows.dauphine.fr>
2019-01-21 12:37:06 +0100

    test commit avec main.java

---
+++ JavaChirurgie/src/Main.java
@@ -0,0 +1,8 @@
+
+public class Main {
+	public static void main(String [] args) {
+		IntervalleTemps intervalle = new IntervalleTemps("01/01/2019", "08:00:01", "01/01/2019", "11:00:00");
+		System.out.println(intervalle);
+                // 
+	}
+}
