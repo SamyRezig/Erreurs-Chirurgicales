a29891ed82abfeba52b8d4674c1542f1de85b9cb
Y.T <38046994+vesran@users.noreply.github.com>
2019-02-24 11:21:20 +0100

    hashCode définit

--- Chirurgien.java
+++ Chirurgien.java
@@ -1,23 +1,28 @@
 
 public class Chirurgien {
 	private String nom;
-	
+
 	public Chirurgien(String nom) {
 		this.nom = nom;
 	}
-	
+
 	public String getNom() {
 		return this.nom;
 	}
-	
+
+	@Override
+	public int hashCode() {
+		return this.nom.hashCode();
+	}
+
 	@Override
 	public boolean equals(Object o) {
 		if (this.getClass() == o.getClass()) {
-			return this.nom.equals(((Chirurgien)o).nom);
+			return this.nom.equals( ( (Chirurgien) o).nom );
 		}
 		return false;
 	}
-	
+
 	@Override
 	public String toString() {
 		return this.nom;
